f657c4e2ae7643f562b399975c02a1a28a93b8f3
Karl Vito Lam <karl.v.lam@gmail.com>
2023-04-11 15:16:44 +0800

    add token-meta

---
+++ wk02-mintcoin/token-meta/whitecard.js
@@ -0,0 +1,12 @@
+let meta = {
+  "name": "WhiteCard",
+  "symbol": "WHC",
+  "image": "https://shdw-drive.genesysgo.net/3LHSXgy6PKo1md3YeyMGDc19sMyeejE3ENzWQbb2YW9F/whitecard.png",
+  "description": "Let's get the White Card!",
+  "creator": {
+    "name": "Mr. Whitecard",
+    "site": "https://whitecard.org"
+  }
+}
+
+console.log(JSON.stringify(meta));
